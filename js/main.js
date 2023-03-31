a1bb90bdbe34f02048e36c804772a09145c12034
Scorpion96522 <126128140+Scorpion96522@users.noreply.github.com>
2023-03-31 16:41:54 +0300

    added styles dot__menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 function clock() {
     const today = new Date();
 
-    const hours = today.getHours();
-    const minutes = today.getMinutes();
-    const seconds = today.getSeconds();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
     let period = "AM";
+
+    // set the Time period AM/PM 
+    if (hours >= 12) {
+        period = "PM";
+    }
+
+    // set the 12-hour clock format 
+    hours = hours > 12 ? hours % 12 : hours;
+
+    // Add the 0 for the values Lower than 10
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
 
     document.querySelector(".hours").innerHTML = hours;
     document.querySelector(".minutes").innerHTML = minutes;
@@ -13,3 +35,14 @@ function clock() {
 
 }
 const updateClock = setInterval(clock, 1000);
+
+const today = new Date();
+const dayNumber = today.getDate();
+const year = today.getFullYear();
+const dayName = today.toLocaleString("default", {weekday: "long"});
+const monthName = today.toLocaleString("default", {month: "short"});
+
+document.querySelector(".month__name").innerHTML = monthName;
+document.querySelector(".day__name").innerHTML = dayName;
+document.querySelector(".day__number").innerHTML = dayNumber;
+document.querySelector(".year").innerHTML = year;
